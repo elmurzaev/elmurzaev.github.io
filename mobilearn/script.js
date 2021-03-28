72a441f343ba3588c3f1c85c1a28d5833ec82e7f
elmurzaev <elmurzaev.ram@gmail.com>
2021-03-28 13:07:49 +0300

    NIght mode

--- mobilearn/script.js
+++ mobilearn/script.js
@@ -1,43 +1,51 @@
-if(localStorage.nightMode == null) localStorage.nightMode = "no"
+if (localStorage.nightMode == null) localStorage.nightMode = 'no'
 
-if(localStorage.nightMode == "night") {
+if (localStorage.nightMode == 'night') {
 	var style = document.getElementById('style')
-	style.href = style.href.replace("style", "style-dark")
+	style.href = style.href.replace('style', 'style-dark')
 }
 
 function enableNightMode() {
-	if(localStorage.nightMode !== "night") {
+	if (localStorage.nightMode !== 'night') {
 		var style = document.getElementById('style')
-		style.href = style.href.replace("style", "style-dark")
-		localStorage.nightMode = "night"
+		style.href = style.href.replace('style', 'style-dark')
+		localStorage.nightMode = 'night'
 		reload()
 	}
 }
 
 function disableNightMode() {
-	localStorage.nightMode = "no"
+	localStorage.nightMode = 'no'
 }
 
-window.onload = function(){
-	if(localStorage.fontSize !== null) initFontSize()
+window.onload = function () {
+	if (localStorage.fontSize !== null) initFontSize()
 }
- 
+
 function initFontSize() {
-	document.body.style.fontSize = localStorage.fontSize + "px"
+	document.body.style.fontSize = localStorage.fontSize + 'px'
 }
 
 function increaseFontSize() {
-	if(localStorage.fontSize == null) localStorage.fontSize = 16
+	if (localStorage.fontSize == null) localStorage.fontSize = 16
 	localStorage.fontSize++
 	initFontSize()
 }
 
 function decreaseFontSize() {
-	if(localStorage.fontSize == null) localStorage.fontSize = 16
+	if (localStorage.fontSize == null) localStorage.fontSize = 16
 	localStorage.fontSize--
 	initFontSize()
 }
 
 function enableTranslator() {
-  new google.translate.TranslateElement({pageLanguage: 'ru', includedLanguages: 'ar,de,en,ru,es,fr,it,uk,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, multilanguagePage: false}, 'google_translate_element');
+	new google.translate.TranslateElement(
+		{
+			pageLanguage: 'ru',
+			includedLanguages: 'ar,de,en,ru,es,fr,it,uk,zh-CN',
+			layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+			multilanguagePage: false,
+		},
+		'google_translate_element',
+	)
 }

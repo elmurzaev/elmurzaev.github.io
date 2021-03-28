ef83c84f17724ef744da251eb805c264f661ed4c
elmurzaev <elmurzaev.ram@gmail.com>
2021-03-28 15:19:14 +0300

    XX

--- mobilearn/script.js
+++ mobilearn/script.js
@@ -1,23 +1,3 @@
-window.onload = function () {
-	if (localStorage.fontSize !== null) initFontSize()
-}
-
-function initFontSize() {
-	document.body.style.fontSize = localStorage.fontSize + 'px'
-}
-
-function increaseFontSize() {
-	if (localStorage.fontSize == null) localStorage.fontSize = 16
-	localStorage.fontSize++
-	initFontSize()
-}
-
-function decreaseFontSize() {
-	if (localStorage.fontSize == null) localStorage.fontSize = 16
-	localStorage.fontSize--
-	initFontSize()
-}
-
 function enableTranslator() {
 	new google.translate.TranslateElement(
 		{

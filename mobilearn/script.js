aeffee2bcd49871a86c2c362c946235b983bedfa
elmurzaev <elmurzaev.ram@gmail.com>
2021-06-10 22:44:54 +0300

    Some changes

--- mobilearn/script.js
+++ mobilearn/script.js
@@ -1,11 +1,11 @@
 function enableTranslator() {
-	new google.translate.TranslateElement(
-		{
-			pageLanguage: 'ru',
-			includedLanguages: 'ar,de,en,ru,es,fr,it,uk,zh-CN',
-			layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-			multilanguagePage: false,
-		},
-		'google_translate_element',
-	)
+    new google.translate.TranslateElement(
+        {
+            pageLanguage: 'ru',
+            includedLanguages: 'ar,de,en,ru,es,fr,it,uk,zh-CN',
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+            multilanguagePage: false,
+        },
+        'google_translate_element',
+    )
 }
